332627590820b02c28c55aa5dfa6907a8b52a178
Charles <charles@cathayholdings.com.tw>
2020-11-08 01:09:30 +0800

    add for loop example

---
+++ forloop/forloop.py
@@ -0,0 +1,14 @@
+arr1 = list(range(2,11,2))
+str1 = 'hello python'
+
+for i in arr1:
+    print(i) # 2 4 6 8 10
+print ('***\n')
+
+for i in range(len(arr1)):
+    print(i) # 0 1 2 3 4 
+
+print ('***\n')
+
+for i in str1:
+    print(i) # h e l l o p y t h o n 
